e6be92d7411f9db2cbd2455e6efaa5eb213e0264
Renato-Alencar <renato.augustoalencar@gmail.com>
2022-01-28 18:57:15 -0300

    Adiciona a rota Sobre

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/home';
-import Contatos from './Pages/contato';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/home";
+import AboutPage from "./Pages/sobre";
+import Contatos from "./Pages/contato";
 
 const MainRoutes = () => {
-    return(
-        <Routes>
-            <Route
-                path="/"
-                element={<HomePage />}
-            />
-            <Route
-                path="/contato"
-                element={<Contatos />}
-            />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/contato" element={<Contatos />} />
+      <Route path="/sobre" element={<AboutPage />} />
+    </Routes>
+  );
+};
 
 export default MainRoutes;

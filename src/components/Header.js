6ce33e240873e83632f9acfad75188e075f27eb0
Renato-Alencar <renato.augustoalencar@gmail.com>
2022-01-28 19:02:24 -0300

    Estiliza o component Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, Typography, Button } from '@material-ui/core/';
-import Cart from './Cart';
+import { Link } from "react-router-dom";
+import { Grid, Typography, Button, makeStyles } from "@material-ui/core/";
+import Cart from "./Cart";
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    textDecorationLine: "none",
+  },
+  button: {
+    marginTop: "15px",
+    color: "#bdc1c6",
+  },
+  typography: {
+    color: "#ffb507",
+  },
+}));
 
 const Header = () => {
-    return(
-        <Grid container direction="row" justify="space-between" alignItems="center" xs={12}>
-            <Typography variant='h3'>
-                Dio Shopping
-            </Typography>
-            <Link to="/">
-                <Button color="primary">Home</Button>
-            </Link>
-            <Link to="/contato">
-                <Button color="primary">Contato</Button>
-            </Link>
-            <Cart />   
-
-            
-        </Grid>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      direction="row"
+      justify="space-around"
+      alignItems="center"
+      xs={12}
+    >
+      <Typography variant="h3" className={classes.typography}>
+        Dio Sports
+      </Typography>
+      <Link to="/" className={classes.link}>
+        <Button color="primary" className={classes.button}>
+          Home
+        </Button>
+      </Link>
+      <Link to="/sobre" className={classes.link}>
+        <Button color="primary" className={classes.button}>
+          Sobre
+        </Button>
+      </Link>
+      <Link to="/contato" className={classes.link}>
+        <Button color="primary" className={classes.button}>
+          Contato
+        </Button>
+      </Link>
+      <Cart />
+    </Grid>
+  );
+};
 
 export default Header;
